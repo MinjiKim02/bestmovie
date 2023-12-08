8801835fcba8809eaebc9e2b37aa18814188d035
minji <minji.kim02@edu.sait.ca>
2023-12-08 14:57:34 -0700

    initial commit
    add some parts and get api

---
+++ app/MovieCard.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const MovieCard = (props) => {
+    return (
+        <div className='card'>
+            <div className='poster'>
+                <img src="" />
+            </div>
+
+            <div className='info'>
+                <p className='title'>{props.title}</p>
+                <p className='vote'>{props.vote_average}</p>
+            </div>
+
+
+
+        </div>
+    )
+}
+
+export default MovieCard
